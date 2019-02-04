192d5e930824354fc06c7b3d9ddace086f703d1b
se-mende <se-mende@uniandes.edu.co>
2019-02-03 21:02:46 -0500

    - Agregada página inicial.

--- apropiacion/urls.py
+++ apropiacion/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('galeria/', include('galeria.urls')),
+    path('', views.index, name='index'),
 ]
