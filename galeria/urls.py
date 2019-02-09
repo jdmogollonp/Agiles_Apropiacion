88f12fa930b2063d65cc8937b62a8b91bc3a8d68
Nicolás Jiménez <estnicjimsae@hotmail.com>
2019-02-09 18:25:57 -0500

    Corrección de carga y listado de imágenes

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path('agregarUsuario/', views.agregar_usuario, name='agregarUsuario'),
     path('registrarUsuario/', views.registrar_usuario, name='registrarUsuario'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
