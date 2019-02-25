f52888b6464c0795e2f97c87258b3a2939fc42f8
jaruano <jairo.ruano@kantartns.com>
2019-02-24 19:04:27 -0500

    fix names

--- apropiacion/settings.py
+++ apropiacion/settings.py
@@ -143,5 +143,5 @@ EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = os.environ['EMAIL_HOST_PASSWORD']#'smtp.gmail.com'
-EMAIL_PORT = os.environ['EMAIL_HOST_PASSWORD']#'587'
+EMAIL_HOST = os.environ['EMAIL_HOST']#'smtp.gmail.com'
+EMAIL_PORT = os.environ['EMAIL_PORT']#'587'
